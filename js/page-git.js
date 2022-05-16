0f6229aa8a9b687066435b0a80f40cbc03e29848
Bruno Gomes <93945369+BrunoBun@users.noreply.github.com>
2022-05-16 17:50:50 -0300

    Update page-git.js

--- js/page-git.js
+++ js/page-git.js
@@ -1,30 +1,20 @@
-getIssues();
-function getIssues() {
-    let fullName = sessionStorage.getItem('fullName');
+getRepositories();
+
+function getRepositories() {
 
     axios({
         method: 'get',
-        url: 'https://api.github.com/repos/'+ fullName +'/issues',
+        url: 'https://api.github.com/repositories',
         responseType: 'stream'
     })
     .then(function (response) {
-        builderIssues(response.data);
+        builderTableRepositories(response.data);
     });
 }
 
-function builderIssues(issues) {
+function builderTableRepositories(repositories) {
     
-    cardsGroup = "";
-    cards = "";
-    indexCard = 0;
-
-    issues.forEach(builderLinesTable);
-
-    console.log(issues);
-
-    if (cards != "") {
-        cardsGroup += "<div class='card-group' style='margin-top: 5px;'>" + cards + "</div>";
-    }
+    repositories.forEach(builderLinesTable);
 
     $("#container-id").html(cardsGroup);
 }
@@ -34,30 +24,39 @@ let cardsGroup = "";
 let indexCard = 0;
 function builderLinesTable(item, index) {
 
-    let status = $("#select-status").val();
-
-    if (status == item.state) {
-
-        indexCard += 1;
-        cards += `
-            <div class="card col-md-4">
-                <img class="card-img-top" src="${item.user.avatar_url}" alt="Avatar url" style="margin-top: 5px;">
-                <div class="card-body">
-                    <h5 class="card-title">User: ${item.user.login}</h5>
-                    <p class="card-text">Title: ${item.title}</p>
-                </div>
-                <div style='align-self: center; margin-bottom: 5px;'>
-                </div>
+    indexCard += 1;
+    cards += `
+        <div class="card col-md-4">
+            <img class="card-img-top" src="${item.owner.avatar_url}" alt="Avatar url" style="margin-top: 5px;">
+            <div class="card-body">
+                <h5 class="card-title">${item.full_name}</h5>
+                <p class="card-text">Description: ${item.description}</p>
             </div>
-        `;
-    
-        if (indexCard == 3) {
-            indexCard = 0;
+            <div style='align-self: center; margin-bottom: 5px;'>
+                <button type="button" class="btn btn-primary" onclick="openDetailOtherPage('${item.full_name}')">Contributors</button>
+                <button type="button" class="btn btn-primary" onclick="openDetailIssues('${item.full_name}')">Issues</button>
+            </div>
+        </div>
+    `;
 
-            cardsGroup += "<div class='card-group' style='margin-top: 5px;'>" + cards + "</div>";
-            cards = "";
-        }
+    if (indexCard == 3) {
+        indexCard = 0;
 
+        cardsGroup += "<div class='card-group' style='margin-top: 5px;'>" + cards + "</div>";
+        cards = "";
     }
-    
+}
+
+function openDetailOtherPage(fullName) {
+
+    sessionStorage.setItem('fullName', fullName);
+
+    window.location="contributors.html";
+}
+
+function openDetailIssues(fullName) {
+
+    sessionStorage.setItem('fullName', fullName);
+
+    window.location="issues.html";
 }
